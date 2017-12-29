212001c575caa62d488bb48a37df33015f1a372d
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2017-12-29 12:42:23 -0800

    Improve CLI code

---
+++ bin/arg-parser.js
@@ -0,0 +1,58 @@
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2017
+ * @license GPL-3.0
+ */
+
+const vonPackage = require('../package.json');
+const ArgumentParser = require('argparse').ArgumentParser;
+const {ALL_GROUPINGS, ALL_ORDERS} = require('../lib/typedef');
+
+const parser = new ArgumentParser({
+    version: vonPackage.version,
+    addHelp: true,
+    description: vonPackage.description,
+    argumentDefault: undefined,
+});
+
+let defineParameter = (name, short, metavar, help, choices = undefined, defaultValue = null) => {
+    parser.addArgument([`-${short}`, `--${name}`], {
+        action: 'store',
+        metavar,
+        help,
+        choices,
+        defaultValue,
+    });
+};
+
+let defineFlag = (name, short, help) => {
+    parser.addArgument([`-${short}`, `--${name}`], {
+        action: 'storeConst',
+        help,
+        constant: true,
+        defaultValue: null,
+    });
+};
+
+defineParameter('directory', 'd', '<path>', 'directory where images are located', undefined, process.cwd());
+defineParameter('output', 'o', '<path>', 'file to which Von HTML will be written');
+defineParameter('template', 't', '<name>', 'built-in template to use');
+defineParameter('template-path', 'tp', '<path>', 'custom template path');
+defineParameter('config', 'c', '<path>', 'custom config path');
+defineParameter('title', 'ti', '<title>', 'title of the gallery');
+defineParameter('description', 'de', '<desc>', 'description of the gallery');
+parser.addArgument(['-g', '--grouping'], {
+    action: 'append',
+    help: 'grouping heuristic for images',
+    choices: ALL_GROUPINGS,
+    defaultValue: null,
+});
+defineParameter('group-order', 'go', undefined, 'group sorting logic', ALL_ORDERS);
+defineParameter('image-order', 'io', undefined, 'image sorting logic', ALL_ORDERS);
+
+defineFlag('defined-groups-only', 'dgo', 'only include the groups from the config');
+defineFlag('recursive', 'r', 'discover images recursively');
+defineFlag('schema', 's', 'print the schema without building the gallery');
+defineFlag('print-options', 'po', 'print command line options without doing anything');
+
+module.exports = parser;
--- bin/von-cli.js
+++ bin/von-cli.js
@@ -8,107 +8,39 @@
  */
 
 const Von = require('../lib/Von');
-const vonPackage = require('../package.json');
-const ArgumentParser = require('argparse').ArgumentParser;
-
-const parser = new ArgumentParser({
-    version: vonPackage.version,
-    addHelp: true,
-    description: vonPackage.description,
-    argumentDefault: undefined,
-});
-parser.addArgument(['-d', '--directory'], {
-    action: 'store',
-    help: 'directory where images are located',
-    defaultValue: process.cwd(),
-});
-parser.addArgument(['-o', '--output'], {
-    action: 'store',
-    help: 'file to which Von HTML will be written',
-    defaultValue: null,
-});
-parser.addArgument(['-t', '--template'], {
-    action: 'store',
-    help: 'built-in template to use',
-    defaultValue: null,
-});
-parser.addArgument(['-tp', '--template-path'], {
-    action: 'store',
-    help: 'custom template path',
-    defaultValue: null,
-});
-parser.addArgument(['-c', '--config'], {
-    action: 'store',
-    help: 'custom config path',
-    defaultValue: null,
-});
-parser.addArgument(['-ti', '--title'], {
-    action: 'store',
-    help: 'title of the gallery',
-    defaultValue: null,
-});
-parser.addArgument(['-de', '--description'], {
-    action: 'store',
-    help: 'description of the gallery',
-    defaultValue: null,
-});
-parser.addArgument(['-dgo', '--defined-groups-only'], {
-    action: 'storeConst',
-    help: 'print the schema without building the gallery',
-    constant: true,
-    defaultValue: null,
-});
-parser.addArgument(['-r', '--recursive'], {
-    action: 'storeConst',
-    help: 'discover images recursively',
-    constant: true,
-    defaultValue: null,
-});
-parser.addArgument(['-g', '--grouping'], {
-    action: 'append',
-    help: 'grouping heuristic for images',
-    defaultValue: null,
-});
-parser.addArgument(['-go', '--group-order'], {
-    action: 'store',
-    help: 'group ordering',
-    defaultValue: null,
-});
-parser.addArgument(['-io', '--image-order'], {
-    action: 'store',
-    help: 'image ordering',
-    defaultValue: null,
-});
-parser.addArgument(['-s', '--schema'], {
-    action: 'storeConst',
-    help: 'print the schema without building the gallery',
-    constant: true,
-    defaultValue: null,
-});
-parser.addArgument(['-po', '--print-options'], {
-    action: 'storeConst',
-    help: 'print command line options without doing anything',
-    constant: true,
-    defaultValue: null,
-});
+const parser = require('./arg-parser');
 
 const args = parser.parseArgs();
 let options = {};
 for (let key in args) {
-    if(!args.hasOwnProperty(key) || args[key] === null) continue;
+    if (!args.hasOwnProperty(key) || args[key] === null) continue;
     let camelized = key.replace(/_./g, s => s.substring(1).toUpperCase());
     options[camelized] = args[key];
 }
 
+let exitWithError = error => {
+    console.error(error);
+    process.exit(1);
+};
+
 if (options.printOptions) {
+
     console.log(options);
+
 } else if (options.schema) {
+
     // Use a magic string to make sure `undefined` is still printed
     let specialString = '%Von-Magic-String$';
     const replacer = (k, v) => v === undefined ? specialString : v;
     Von.generateSchema(options)
-        .then(schema => console.log(JSON.stringify(schema, replacer, 2).replace(new RegExp(`"${specialString}"`, 'g'), 'undefined')));
+        .then(schema => console.log(JSON.stringify(schema, replacer, 2).replace(new RegExp(`"${specialString}"`, 'g'), 'undefined')))
+        .then(() => process.exit(0))
+        .catch(exitWithError);
+
 } else {
+
     Von.run(options)
-        .catch(error => console.error(error));
+        .then(() => process.exit(0))
+        .catch(exitWithError);
+
 }
